e0a2d263b9c42fde01a317328590f6b98b1b60c4
luish000 <luish000.lh@gmail.com>
2016-03-01 15:31:46 -0430

    Updated to 1.0.2

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 'use strict'
 
 /**
- * Base Controller
- * Inherit of this class for create controllers
+ * @class
+ * @classdesc this class define a proton policy
+ * @author Luis Hernandez
  */
-module.exports = class ProtonPolicy {
+ class ProtonPolicy {
 
   constructor(proton) {
-    this.proton = proton
-    this._bindToApp()
-    this.expose()
+    super(proton)
   }
 
-  /**
-   * @return class name
-   */
+  expose(policy) {
+    global[this.name] = policy
+  }
+
   get name() {
     return this.constructor.name
   }
 
-  /**
-   * @description Expose this controller globally,
-   * Overwrite and return false for not expose the controller
-   *
-   * @return true | false
-   */
-  expose() {
-    global[this.name] = this
-    return true
-  }
-
-  /**
-   * @description Bind this controller to the app.controllers objects
-   */
-  _bindToApp() {
-    this.proton.app.controllers[this.name] = this
+  set fileName(fileName) {
+    this.fileName = fileName
   }
 
 }
